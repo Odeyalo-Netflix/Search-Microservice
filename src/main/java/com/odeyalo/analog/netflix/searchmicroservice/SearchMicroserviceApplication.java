10bfe3423fa17fb15717a5405bf4fa8879fb0c23
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-15 21:42:10 +0300

    Main class

---
+++ src/main/java/com/odeyalo/analog/netflix/searchmicroservice/SearchMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.odeyalo.analog.netflix.searchmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchMicroserviceApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(SearchMicroserviceApplication.class, args);
+    }
+}
