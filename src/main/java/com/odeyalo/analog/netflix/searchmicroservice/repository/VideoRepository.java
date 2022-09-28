7fb4124d0be3e80b22821bb780b81215421ef40c
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-28 15:53:35 +0300

    It's ReactiveElasticsearchRepository child interface now

--- src/main/java/com/odeyalo/analog/netflix/searchmicroservice/repository/VideoRepository.java
+++ src/main/java/com/odeyalo/analog/netflix/searchmicroservice/repository/VideoRepository.java
@@ -1,12 +1,12 @@
 package com.odeyalo.analog.netflix.searchmicroservice.repository;
 
 import com.odeyalo.analog.netflix.searchmicroservice.entity.Video;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.elasticsearch.repository.ReactiveElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
 @Repository
-public interface VideoRepository extends ReactiveCrudRepository<Video, Long> {
+public interface VideoRepository extends ReactiveElasticsearchRepository<Video, Long> {
 
     Flux<Video> findVideoByName(String name);
 
