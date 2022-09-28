4bc51d84a3fa731cf5086189095c1c38aaed255c
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-28 15:53:50 +0300

    Added EnableReactiveElasticsearchRepositories annotation

--- src/main/java/com/odeyalo/analog/netflix/searchmicroservice/SearchMicroserviceApplication.java
+++ src/main/java/com/odeyalo/analog/netflix/searchmicroservice/SearchMicroserviceApplication.java
@@ -2,11 +2,13 @@ package com.odeyalo.analog.netflix.searchmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableReactiveElasticsearchRepositories;
 
 @SpringBootApplication
+@EnableReactiveElasticsearchRepositories
 public class SearchMicroserviceApplication {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SpringApplication.run(SearchMicroserviceApplication.class, args);
     }
 }
