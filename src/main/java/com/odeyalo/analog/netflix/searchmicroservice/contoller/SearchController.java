035471b0c6ae43f1da45f0987e6ae81d7af4bd6e
Odeyalo <theprogrammerinfuture@gmail.com>
2022-09-29 18:14:54 +0300

    Added SearchVideoManager to working access service layer

--- src/main/java/com/odeyalo/analog/netflix/searchmicroservice/contoller/SearchController.java
+++ src/main/java/com/odeyalo/analog/netflix/searchmicroservice/contoller/SearchController.java
@@ -4,9 +4,11 @@ import com.odeyalo.analog.netflix.searchmicroservice.dto.SearchVideoResponseDTO;
 import com.odeyalo.analog.netflix.searchmicroservice.dto.VideoSuggestResponseDTO;
 import com.odeyalo.analog.netflix.searchmicroservice.entity.Video;
 import com.odeyalo.analog.netflix.searchmicroservice.repository.VideoRepository;
+import com.odeyalo.analog.netflix.searchmicroservice.service.SearchVideoManager;
 import com.odeyalo.analog.netflix.searchmicroservice.service.VideoSearcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -19,42 +21,49 @@ import java.util.stream.Collectors;
 @CrossOrigin("*")
 public class SearchController {
     private final VideoRepository repository;
+    private final SearchVideoManager manager;
     private final VideoSearcher searcher;
     private final Logger logger = LoggerFactory.getLogger(SearchController.class);
+
+
+    @Autowired
+    public SearchController(VideoRepository repository, SearchVideoManager manager, VideoSearcher searcher) {
+        this.repository = repository;
+        this.manager = manager;
+        this.searcher = searcher;
+    }
 
     @GetMapping("/video/all")
     public Flux<Video> list() {
         return this.repository.findAll();
     }
 
-    public SearchController(VideoRepository repository, VideoSearcher searcher) {
-        this.repository = repository;
-        this.searcher = searcher;
-    }
 
     @GetMapping("/suggest")
-    public Mono<List<VideoSuggestResponseDTO>> suggest(@RequestParam(name = "q") String query) {
+    public Mono<List<VideoSuggestResponseDTO>> suggest(@RequestParam(name = "q") String query, @RequestParam(defaultValue = "default") boolean autoComplete) {
+        if (autoComplete) {
+            return this.searcher.findAllByQueryWithCompletion(query).map(x -> x.stream().map(video -> new VideoSuggestResponseDTO(video.getName())).collect(Collectors.toList()));
+
+        }
         return this.searcher.findAllByQuery(query).map(x -> x.stream().map(video -> new VideoSuggestResponseDTO(video.getName())).collect(Collectors.toList()));
+    }
+
+    @GetMapping("/video")
+    public Flux<SearchVideoResponseDTO> searchByQuery(@RequestParam(name = "q") String query) {
+        return this.manager.getVideo(query, true).map(video -> {
+            SearchVideoResponseDTO response = new SearchVideoResponseDTO();
+            response.fromVideo(video);
+            return response;
+        });
     }
 
     @PostMapping("/save")
     public Mono<?> save(@RequestBody Video video) {
-        return this.repository.save(video);
+        return this.manager.saveVideo(video);
     }
 
     @PostMapping("/saveAll")
     public Flux<?> saveAll(@RequestBody List<Video> videos) {
         return this.repository.saveAll(videos);
-    }
-
-    @GetMapping("/video")
-    public Mono<List<SearchVideoResponseDTO>> searchByQuery(@RequestParam(name = "q") String query) {
-        return this.searcher.findAllByQuery(query).map(mono -> {
-            return mono.stream().map(video -> {
-                SearchVideoResponseDTO response = new SearchVideoResponseDTO();
-                response.fromVideo(video);
-                return response;
-            }).collect(Collectors.toList());
-        });
     }
 }
